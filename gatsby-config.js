1f89d8c0fe208517a3a383274f1878777cdacaad
gigarthan <gigarthan@parkwaylabs.com>
2019-05-17 19:11:12 +0530

    Add portfolio home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gigarthan`,
+    description: `Personal Porfolio and Blog`,
+    author: `@gigarthan`,
   },
   plugins: [
+    `gatsby-plugin-postcss`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -25,6 +26,17 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Fira Mono`,
+            variants: [`400`],
+          },
+        ],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ src/components/home/home.js
@@ -0,0 +1,67 @@
+import React from "react"
+import "../../css/global.css"
+import "./home.css"
+import { Link } from "gatsby"
+
+export default () => (
+  <React.Fragment>
+    <section>
+      <section className="flex">
+        <section className="home-card w-full bg-white rounded m-4 shadow-lg flex flex-wrap justify-center overflow-hidden">
+          <div className="mt-32 content">
+            <h1 className="my-font">Hi, I'm Gigarthan; Web Dev; JS Lover</h1>
+            <div className="hr-line" />
+            <div className="nav">
+              <ul className="flex list-none justify-center my-font">
+                <li className="m-2 mb-0">
+                  <div className="li">
+                    <p>
+                      <a
+                        className="no-underline text-black"
+                        href="https://medium.com/@gigarthan"
+                        target="_blank"
+                      >
+                        Blog
+                      </a>
+                    </p>
+                    <div className="li-line" />
+                  </div>
+                </li>
+                <li className="m-2 mb-0">
+                  <div className="li">
+                    <p>Projects</p>
+                    <div className="li-line" />
+                  </div>
+                </li>
+                <li className="m-2 mb-0">
+                  <div className="li">
+                    <p>
+                      <a
+                        className="no-underline text-black"
+                        href="https://twitter.com/gigarthan"
+                        target="_blank"
+                      >
+                        Twitter
+                      </a>
+                    </p>
+                    <div className="li-line" />
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <section className="lines">
+            <div className="line line-1" />
+            <div className="line line-2" />
+            <div className="line line-3" />
+            <div className="line line-4" />
+            <div className="line line-5" />
+            <div className="line line-6" />
+            <div className="line line-7" />
+            <div className="line line-8" />
+          </section>
+        </section>
+      </section>
+    </section>
+  </React.Fragment>
+)
